cd1219abdcf4b82ad5a361470731431d30c12e1f
TestName{UFO} <36362763+testufo@users.noreply.github.com>
2020-06-30 22:38:54 +0300

    Update newMod.Designer.cs

--- RWS/newMod.Designer.cs
+++ RWS/newMod.Designer.cs
@@ -326,6 +326,7 @@
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ForeColor = System.Drawing.Color.Silver;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
